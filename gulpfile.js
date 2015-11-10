61cec69e031170512462f4c16422662eb2f2b8b9
barradas <joaoppbarradas@gmail.com>
2015-11-10 16:19:12 +0100

    created a concatenation task using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    concat = rquire('gulp-concat');
 
+var jsSources = [
+     'components/js/script.js',
+     'components/js/script.js',
+     'components/js/script.js'
+];
+
+gulp.task('log', function (){
+    gutil.log('gulp is amazing');
+
+});
+
+gulp.task('js', function(){
+
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('build/development/js'))
+});
